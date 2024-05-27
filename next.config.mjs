59fc4093abf1b6be232af8693f6ccac69a8d1ea5
devansh-kc <devansh.kc96@gmail.com>
2024-05-28 04:04:25 +0530

    made change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
-module.exports = {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
